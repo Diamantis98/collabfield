a5521346e1925e948e3104d76b71db551d3201c5
her_username <dim-1998@hotmail.com>
2020-02-22 20:12:16 +0200

    - Add the bootstrap layout to the home page - Add CSS to make home page layout's stylistic and responsive design changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
   context 'Associations' do
     it 'belongs_to user' do
       association = described_class.reflect_on_association(:user).macro
