c4dd7efa9032cd860b655cbb4a618cb1a560545e
her_username <dim-1998@hotmail.com>
2020-02-25 17:18:16 +0200

    Fixed configurations and environment

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,11 @@ Bundler.require(*Rails.groups)
 module Collabfield
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
-    config.action_controller.include_all_helpers = false
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.action_controller.include_all_helpers = false
   end
 end
